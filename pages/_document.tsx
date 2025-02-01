e8771e4a4b60e8920dab3a86e09e6f21ff611803
euijinkk <gytks4@naver.com>
2025-02-01 15:50:59 +0900

    chore: load manifest in head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="PWA Practice with Push Notifications"
+        />
+        <link rel="apple-touch-icon" href="/assets/images/logo192.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
